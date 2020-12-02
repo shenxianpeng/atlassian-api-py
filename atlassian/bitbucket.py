d5d6b3de6c8e66f12e7ff550d1bfaa00c968e4d9
shenxianpeng <xianpeng.shen@gmail.com>
2020-12-02 19:23:17 +0800

    feat(bitbucket) add branches and pull requests releated fucntions

--- atlassian/bitbucket.py
+++ atlassian/bitbucket.py
@@ -1,4 +1,5 @@
 import configparser
+import re
 from atlassian.client import AtlassianAPI
 from atlassian.logger import get_logger
 
@@ -77,13 +78,13 @@ class Bitbucket(AtlassianAPI):
         return self.delete(url, json=json)
 
     def get_merged_branch(self, project_key, repo_key, start=0, limit=None):
-        url = '/rest/api/latest/projects/{0}/repos/{1}/branches?base=refs/heads/master'.format(project_key, repo_key)
+        url = '/rest/api/latest/projects/{0}/repos/{1}/branches?base=refs/heads/master&details=true'.format(project_key, repo_key)
         params = {}
         if start:
             params["start"] = start
         if limit:
             params['limit'] = limit
-        return self._get_paged(url, params=params)
+        branches = self._get_paged(url, params=params)
 
         merged_branches = []
         for branch in branches:
@@ -94,7 +95,70 @@ class Bitbucket(AtlassianAPI):
                                 and metadata_value['pullRequest']['state'] == 'MERGED':
                             if branch['displayId'] not in merged_branches:
                                 merged_branches.append(branch['displayId'])
-                                return merged_branches
+        return merged_branches
+
+    def get_branch_commits(self, project_key, repo_key, branch_name, start=0, limit=None):
+        url = '/rest/api/latest/projects/{0}/repos/{1}/commits/?until={2}'.format(project_key, repo_key, branch_name)
+        params = {}
+        if start:
+            params["start"] = start
+        if limit:
+            params['limit'] = limit
+        return self._get_paged(url, params=params)
+
+    def get_branch_committer_info(self, project_key, repo_key, branch_name, start=0, limit=None):
+        commits = self.get_branch_commits(project_key, repo_key, branch_name, start=start, limit=limit)
+        committer = []
+        for commit in commits:
+            committer.append(commit['committer'])
+        return committer
+
+    def get_pull_request(self, project_key, repo_key, pr_state="ALL", start=0, limit=None):
+        """
+        :param project_key:
+        :param repo_key:
+        :param pr_state: ALL, OPEN, MERGED, DECLINED
+        :param start:
+        :param limit:
+        :return:
+        """
+        url = '/rest/api/latest/projects/{0}/repos/{1}/pull-requests?state={2}'.format(project_key, repo_key, pr_state)
+        params = {}
+        if start:
+            params["start"] = start
+        if limit:
+            params['limit'] = limit
+        return self._get_paged(url, params=params)
+
+    def get_pull_request_source_branch_name(self, project_key, repo_key, pr_id):
+        index = 0
+        while True:
+            index += 25
+            prs = self.get_pull_request(project_key, repo_key, limit=index)
+            for pr in prs:
+                if pr['id'] == int(pr_id):
+                    return pr['fromRef']['displayId']
+                else:
+                    return None
+
+    def get_pull_request_destination_branch_name(self, project_key, repo_key, pr_id):
+        index = 0
+        while True:
+            index += 25
+            prs = self.get_pull_request(project_key, repo_key, limit=index)
+            for pr in prs:
+                if pr['id'] == int(pr_id):
+                    return pr['toRef']['displayId']
+                else:
+                    return None
+
+    def get_pull_request_relate_jira_key(self, project_key, repo_key, pr_id):
+        source_branch_name = self.get_pull_request_source_branch_name(project_key, repo_key, pr_id)
+        try:
+            jira_key = re.search(r'[A-Z]+-[0-9]+', source_branch_name).group()
+            return jira_key
+        except AttributeError as e:
+            logger.error(e)
 
 
 if __name__ == '__main__':
@@ -106,5 +170,5 @@ if __name__ == '__main__':
     git_psw = config['bitbucket']['password']
 
     git = Bitbucket(url=git_url, username=git_usr, password=git_psw)
-    ret = git.get_merged_branch("MVAS", "uvuddb")
+    ret = git.get_pull_request_source_branch_name("MVAS", "uvuddb", '668')
     print(ret)
