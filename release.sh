3b3733f01f4ee368476dcec796e1c0a1b310e5b3
shenxianpeng <xianpeng.shen@gmail.com>
2020-12-16 12:02:36 +0800

    refactor codes base on codefactor scan

--- release.sh
+++ release.sh
@@ -12,13 +12,15 @@ check_exec_result()
 
 bump_version()
 {
-  read -p "   atlassian-api-py current version is ? " old_version
-  read -p "   atlassian-api-py increase version to ? " new_version
+  echo "current version is ?"
+  read -r old_version
+  echo "increase version to ?"
+  read -r new_version
   old_revision=`echo $old_version | cut -d. -f3`
   new_revision=`echo $new_version | cut -d. -f3`
   sed -i "s/$old_revision/$new_revision/g" setup.py
-  check_result
-  echo "[√] completed bump version. new version is:"
+  check_exec_result
+  echo "[√] bump version successful, new version is:"
   grep "version" setup.py
   check_exec_result
 }
@@ -28,12 +30,12 @@ git_push()
   current_branch=`git branch --show-current`
   if [ ${current_branch} = "master" ]; then
     git add setup.py
-    git commit -m "Bump atlassian-api-py version to $new_revision"
+    git commit -m "Bump version to $new_revision"
     git push origin master
     git tag $version
     git push --tag
   else
-    echo "[X] please switch to master branch first."
+    echo "[X] Please switch to master branch first."
     exit
   fi
 }
@@ -57,7 +59,8 @@ check_exec_result
 echo
 
 while true; do
-  read -p "1. Do you need to upgrade version ? " yn
+  echo "1. Do you need to upgrade version ?"
+  read -r yn
   case $yn in
     [Yy]* ) bump_version ; break ;;
     [Nn]* ) echo; echo "[*] skip upgrade version"; echo; break;;
@@ -66,7 +69,8 @@ while true; do
 done
 
 while true; do
-  read -p "2. Ready to push version changes to GitHub ? " yn
+  echo "2. Ready to push version changes to GitHub ?"
+  read -r yn
   case $yn in
     [Yy]* ) git_push ; break ;;
     [Nn]* ) echo; echo "[*] skip push to Git"; echo; break;;
@@ -75,7 +79,8 @@ while true; do
 done
 
 while true; do
-  read -p "3. Ready to release to PyPI ? " yn
+  echo "3. Ready to release to PyPI ?"
+  read -r yn
   case $yn in
     [Yy]* ) release_to_PyPI ; break ;;
     [Nn]* ) echo; echo "[*] skip deploy to PyPI for release"; echo; break;;
