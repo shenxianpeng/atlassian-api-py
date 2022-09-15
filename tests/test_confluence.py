d06f8eb26938f91504974feb4681dfcd8f538f50
shenxianpeng <xianpeng.shen@gmail.com>
2022-09-15 13:49:20 +0800

    feat: save changes

--- tests/test_confluence.py
+++ tests/test_confluence.py
@@ -2,8 +2,8 @@ import unittest
 import configparser
 from atlassian import Confluence
 
-config = configparser.ConfigParser()
-config.read("config.ini")
+config = configparser.RawConfigParser()
+config.read("tests/config.ini")
 
 wiki_url = config["confluence"]["url"]
 wiki_usr = config["confluence"]["username"]
@@ -12,12 +12,34 @@ wiki_psw = config["confluence"]["password"]
 
 class TestConfluence(unittest.TestCase):
     """Confluence Test Cases"""
+
     def setUp(self):
         """Set connection with Confluence"""
-        print("hello from wiki")
         self.wiki = Confluence(url=wiki_url, username=wiki_usr, password=wiki_psw)
-        print("hello from wiki")
 
     def test_get_content(self):
         content = self.wiki.get_content()
-        print("hello from wiki")
+        limit = content.limit
+        assert(limit, 25)
+
+    def test_operate_content(self):
+        # test create_content
+        response = self.wiki.create_content(title="My Test Page", space_key="~xshen", body_value="<p>This is a new page</p>")
+        assert(response['status'], "current")
+
+        # test update_content
+        page_id = response['id']
+        a = self.wiki.update_content(page_id, title="My Test Page Update", body_value="<p>This is a update page</p>")
+        
+        # test get_content_by_id
+        page_content = self.wiki.get_content_by_id(page_id)
+        assert(page_content.title, "My Test Page Update")
+
+        # test get_content_history
+        page_history = self.wiki.get_content_history(page_id)
+        assert(page_history.latest, 'true')
+
+        # test delete_content
+        self.wiki.delete_content(page_id)
+        page_content = self.wiki.get_content_by_id(page_id)
+        assert(page_content.statusCode, 404)
