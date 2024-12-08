f19d118111001ead70de03600d316457ed9772e4
shenxianpeng <xianpeng.shen@gmail.com>
2024-12-08 23:28:16 +0200

    fix: comment out coverage session

--- noxfile.py
+++ noxfile.py
@@ -35,12 +35,12 @@ def test(session):
     session.run("pytest")
 
 
-@nox.session()
-def coverage(session):
-    if os.getenv("GITHUB_ACTIONS") == "true":
-        nox.skip("Skipping this session because it failed on GitHub Actions.")
-    session.install(".")
-    session.install("-r", "requirements-dev.txt")
-    session.run("coverage", "run", "-m", "unittest")
-    session.run("coverage", "report", "-m")
-    session.run("coverage", "html")
+# @nox.session()
+# def coverage(session):
+#     if os.getenv("GITHUB_ACTIONS") == "true":
+#         nox.skip("Skipping this session because it failed on GitHub Actions.")
+#     session.install(".")
+#     session.install("-r", "requirements-dev.txt")
+#     session.run("coverage", "run", "-m", "unittest")
+#     session.run("coverage", "report", "-m")
+#     session.run("coverage", "html")
