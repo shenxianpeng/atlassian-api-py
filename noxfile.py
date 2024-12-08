6173c82c656609ebb117b2e180b9b0fdb2ff4a58
shenxianpeng <xianpeng.shen@gmail.com>
2024-12-08 22:49:46 +0200

    fix: drop supporting py38

--- noxfile.py
+++ noxfile.py
@@ -28,7 +28,7 @@ def install(session):
     session.install(*whl_file)
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
 def test(session):
     session.install(".")
     session.install("-r", "requirements-dev.txt")
