7715f248c9251d782be480bd2ca720681a351606
shenxianpeng <xianpeng.shen@gmail.com>
2024-12-08 21:46:40 +0200

    feat: replace makefile to noxfile.py

---
+++ noxfile.py
@@ -0,0 +1,39 @@
+import nox
+import glob
+
+nox.options.reuse_existing_virtualenvs = True
+nox.options.error_on_missing_interpreters = False
+
+
+@nox.session
+def lint(session):
+    session.install("-r", "requirements-dev.txt")
+    session.run("pre-commit", "run", "--all-files")
+    session.run("mypy", "atlassian")
+
+
+@nox.session
+def build(session):
+    session.run("python3", "-m", "pip", "wheel", "-w", "dist", "--no-deps", ".")
+
+
+@nox.session
+def install(session):
+    whl_file = glob.glob("dist/atlassian_api_py*.whl")
+    session.install(*whl_file)
+
+
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"])
+def test(session):
+    session.install(".")
+    session.install("-r", "requirements-dev.txt")
+    session.run("pytest")
+
+
+@nox.session()
+def coverage(session):
+    session.install(".")
+    session.install("-r", "requirements-dev.txt")
+    session.run("coverage", "run", "-m", "unittest")
+    session.run("coverage", "report", "-m")
+    session.run("coverage", "html")
